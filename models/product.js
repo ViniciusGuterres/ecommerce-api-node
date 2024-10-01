1615fef30a3098d9373e0b7d60f688789b8c68c1
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-01 15:25:01 -0300

    Added id column

--- models/product.js
+++ models/product.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const Product = sequelize.define('Product', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false,
