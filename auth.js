49f750b70d8e336a133926a240abf45be24781f8
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-02 20:59:52 -0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,64 @@
+const crypto = require('crypto');
+const { promisify } = require('util');
+
+const scrypt = promisify(crypto.scrypt);
+
+const jwt = require('jsonwebtoken');
+const secret = 'mySecretKey';
+
+async function generateToken(user){
+    const id = user.id;
+    const email = user.email;
+    const token = jwt.sign({id,email},secret, {expiresIn:'1h'});
+    return token;
+}
+
+async function verifyToken(req, res, next){
+    const authHeader =req.headers['authorization'];
+    if(!authHeader){
+        return res.status(401).json({Message:'Token não informado'});
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({message:'Token não informado'});
+    }
+
+    jwt.verify(token, secret,(err, decoded)=>{
+        if (err){
+            return res.status(401).json({Message:'Token inválido'});
+        }
+        req.user = decoded;
+        next();
+    });
+}
+
+async function hashPassword(password) {
+    // Generate a salt (random string of numbers and letters)
+    const salt = crypto.randomBytes(8).toString('hex');
+
+    // Hash the salt and the password together
+    const hash = await scrypt(password, salt, 32);
+
+    // Join the hashed result and the salt together
+    const hashPassword = `${salt}.${hash.toString('hex')}`;
+
+    return hashPassword;
+}
+
+module.exports = {generateToken, verifyToken, hashPassword} ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
