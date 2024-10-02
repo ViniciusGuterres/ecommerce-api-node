f49c121e5bd98b9b676b987a689f0279abe244d4
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-02 19:41:06 -0300

    Renaming const

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,11 +4,11 @@ const ProductService = require('../services/productService');
 const ProductController = require('../controllers/productController');
 
 const productService = new ProductService();
-const product = new ProductController(productService);
+const productController = new ProductController(productService);
 
 module.exports = app => {
-    app.post('/products', bodyParser.json(), product.createProduct);
-    app.get('/products', bodyParser.json(), product.getAllProducts);
-    app.put('/products/:id', bodyParser.json(), product.updateProduct);
-    app.delete('/products/:id', bodyParser.json(), product.deleteProduct);
+    app.post('/products', bodyParser.json(), productController.createProduct);
+    app.get('/products', bodyParser.json(), productController.getAllProducts);
+    app.put('/products/:id', bodyParser.json(), productController.updateProduct);
+    app.delete('/products/:id', bodyParser.json(), productController.deleteProduct);
 }
