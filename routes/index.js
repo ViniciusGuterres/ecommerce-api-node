ad7fa2f962c867b5055b65d79f899b9b173b884f
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-02 19:16:21 -0300

    Added users, products and shopping routes to the index router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
-const myTestRoute = require('./myRoute.js');
+const usersRoutes = require('./userRoutes.js');
+const productsRoutes = require('./productRoutes.js');
+const shoppingCartsRoutes = require('./shoppingCartRoutes.js');
 
 module.exports = app => {
-    myTestRoute(app);
+    app.get('/', (req, res) => {
+        res.status(200).send('Hello world!');
+    })
+
+    usersRoutes(app);
+    productsRoutes(app);
+    shoppingCartsRoutes(app);
     
     // Applying a 404 route handler for all methods
     app.use((req, res) => {
