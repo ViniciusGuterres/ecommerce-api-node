31695a4d186829882097404f7983421f65a16b8e
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-02 20:45:41 -0300

    Update shoppingCartController.js

--- controllers/shoppingCartController.js
+++ controllers/shoppingCartController.js
@@ -44,19 +44,29 @@ class ShoppingCartController {
     }
 
     removeItemFromCart = async (req, res) => {
+        const returnObj = { statusCode: 200, err: null, data: null };
+
         const { userId } = req.body;
         const { id: productId } = req.params;
 
         if (!userId || !productId) {
-            return res.status(400).json({ error: 'userId and productId are required' });
+            returnObj.statusCode = 400;
+            returnObj.err = 'userId and productId data are required';
+
+            console.log(`controllers/shoppingCartController.removeItemFromCart - ${returnObj.err}`);
+            this.sendResponse(res, returnObj.statusCode, returnObj);
+            return;
         }
 
         try {
             const cart = await this.shoppingCartService.removeItemFromCart(userId, productId);
-            return res.status(200).json(cart);
+            returnObj.data = cart;
         } catch (error) {
-            return res.status(500).json({ error: error.message });
+            returnObj.statusCode = 500;
+            returnObj.err = error;
         }
+
+        return this.sendResponse(res, returnObj.statusCode, returnObj);
     }
 
     getCart = async (req, res) => {
