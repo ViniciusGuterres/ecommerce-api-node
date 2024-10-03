10f27c4cfc10e1bc66cbc160887ac1e894f7f260
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-02 21:20:57 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 class User {
-    constructor(UserService){
+    constructor(UserService) {
         this.userService = UserService;
     }
 
@@ -21,7 +21,7 @@ class User {
             returnObj.statusCode = 400;
             returnObj.err = 'No body received or body empty';
 
-            console.log(`controllers/user.signUp - ${returnObj.err}`);
+            console.log(`controllers/userController.signUp - ${returnObj.err}`);
             this.sendResponse(res, returnObj.statusCode, returnObj);
             return;
         }
@@ -32,7 +32,7 @@ class User {
             returnObj.statusCode = 400;
             returnObj.err = 'name no received or type is not a string';
 
-            console.log(`controllers/user.signUp - ${returnObj.err}`);
+            console.log(`controllers/userController.signUp - ${returnObj.err}`);
             this.sendResponse(res, returnObj.statusCode, returnObj);
             return;
         }
@@ -41,7 +41,7 @@ class User {
             returnObj.statusCode = 400;
             returnObj.err = 'email no received or type is not a string';
 
-            console.log(`controllers/user.signUp - ${returnObj.err}`);
+            console.log(`controllers/userController.signUp - ${returnObj.err}`);
             this.sendResponse(res, returnObj.statusCode, returnObj);
             return;
         }
@@ -50,7 +50,7 @@ class User {
             returnObj.statusCode = 400;
             returnObj.err = 'email format not valid';
 
-            console.log(`controllers/user.signUp - ${returnObj.err}`);
+            console.log(`controllers/userController.signUp - ${returnObj.err}`);
             this.sendResponse(res, returnObj.statusCode, returnObj);
             return;
         }
@@ -59,7 +59,7 @@ class User {
             returnObj.statusCode = 400;
             returnObj.err = 'password no received or type is not a string';
 
-            console.log(`controllers/user.signUp - ${returnObj.err}`);
+            console.log(`controllers/userController.signUp - ${returnObj.err}`);
             this.sendResponse(res, returnObj.statusCode, returnObj);
             return;
         }
@@ -70,7 +70,7 @@ class User {
             returnObj.statusCode = 500;
             returnObj.err = err;
 
-            console.log(`controllers/user.signUp - ${returnObj.err}`);
+            console.log(`controllers/userController.signUp - ${returnObj.err}`);
             this.sendResponse(res, returnObj.statusCode, returnObj);
             return;
         }
@@ -80,10 +80,45 @@ class User {
         this.sendResponse(res, returnObj.statusCode, returnObj);
     }
 
-    async signIn(req, res) {
-        console.log('signIn::');
+    signIn = async (req, res) => {
+        const returnObj = { statusCode: 200, err: null, data: null };
 
-        res.status(200).send('working');
+        // Req.body validations
+        if (!req.body || !Object.values(req.body)?.length) {
+            returnObj.statusCode = 400;
+            returnObj.err = 'No body received or body empty';
+
+            console.log(`controllers/userController.signIn - ${returnObj.err}`);
+            this.sendResponse(res, returnObj.statusCode, returnObj);
+            return;
+        }
+
+        const { email, password } = req.body;
+
+        // Req.body validations
+        if (!email || !password) {
+            returnObj.statusCode = 400;
+            returnObj.err = 'No email or password received';
+
+            console.log(`controllers/userController.signIn - ${returnObj.err}`);
+            this.sendResponse(res, returnObj.statusCode, returnObj);
+            return;
+        }
+
+        const { err, data } = await this.userService.signIn(email, password);
+
+        if (err) {
+            returnObj.statusCode = 500;
+            returnObj.err = err;
+
+            console.log(`controllers/userController.signIn - ${returnObj.err}`);
+            this.sendResponse(res, returnObj.statusCode, returnObj);
+            return;
+        }
+
+        returnObj.data = data;
+
+        this.sendResponse(res, returnObj.statusCode, returnObj);
     }
 }
 
